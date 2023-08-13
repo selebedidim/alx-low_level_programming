fe05cc063befa2ee05a7f9a4956cb30752275e46
Dimakatso Selebedi <selebedidimakatso083357@gmail.com>
2023-08-13 13:55:53 -0700

    Patience persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,14 @@ int main(void)
 {
 	char d = '0';
 
-	while (d <= '8')
+	while (d <= '9')
 	{
 		putchar(d);
+		if (d  <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		d++;
 	}
 	putchar('\n');
