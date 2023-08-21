aea06a62ad6490f42f0d893d970b5e99aeba8a71
Dimakatso Selebedi <selebedidimakatso083357@gmail.com>
2023-08-21 12:04:48 -0700

    98 Battery st

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -27,7 +27,8 @@ void print_triangle(int size)
 			}
 				_putchar('\n');
 			}
-		}
-	_putchar('\n');
+		
+	}
+
 
 }
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of the points to 98
+ * @n: Pointer to int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
