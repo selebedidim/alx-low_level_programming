17215fad9073e236e1fef4487aa8dd9e2667ad8a
Dimakatso Selebedi <selebedidimakatso083357@gmail.com>
2023-08-29 13:50:07 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,6 @@
  * _strchr - locates a character in a string
  * @s:string to be searched
  * @c:character to be located
- *
  * Return:if c is found - pointer to the first occurence.
  *        if c is not found return null
  */
@@ -12,6 +11,7 @@
 char *_strchr(char *s, char c)
 {
 	int index;
+
 	for (index = 0; s[index] >= '\0'; index++)
 	{
 		if (s[index] == c)
