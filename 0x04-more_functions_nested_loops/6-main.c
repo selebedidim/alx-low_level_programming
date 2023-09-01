93a0414fefbbf13ed5c3767c4bcab3a2a9edaa82
Dimakatso Selebedi <selebedidimakatso083357@gmail.com>
2023-09-01 15:19:56 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x04-more_functions_nested_loops/6-main.c
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,9 +8,15 @@
  */
 int main(void)
 {
-		print_line(0);
-		print_line(2);
-		print_line(10);
-		print_line(-4);
+		int r;
+
+		r = _abs(-1);
+		printf("%d\n", r);
+		r = _abs(0);
+		printf("%d\n", r);
+		r = _abs(1);
+		printf("%d\n", r);
+		r = _abs(-98);
+		printf("%d\n", r);
 		return (0);
 }
