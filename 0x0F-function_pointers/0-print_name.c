5c37ac152e113b5a609e9492ae611a4674f33a51
Dimakatso Selebedi <selebedidimakatso083357@gmail.com>
2023-09-13 14:32:56 -0700

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
- * print_name - print name using pointer to function
- * @name:name to add
- * @f:pointer to function printed name
+ * print_name - print name using a pointer to function
+ * @name:name to be printed
+ * @f:pointer to a function that printed name
  *
  * Return:0
  */
@@ -14,4 +13,3 @@ void print_name(char *name, void (*f)(char *))
 		return;
 	f(name);
 }
-
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,9 @@
-#ifndef  HEADER_FILE
-#define  HEADER_FILE
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
 #include <stdio.h>
-int _putchar(char c);
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-
 #endif
