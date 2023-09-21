578b1cf60cbbb0acc1b1ca7ad13783090e981258
Dimakatso Selebedi <selebedidimakatso083357@gmail.com>
2023-09-21 13:41:04 -0700

    function that prints You're beat!

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void first(void)__attribute__((constructor));
+/**
+ * first - ...
+ *
+ * return:nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
