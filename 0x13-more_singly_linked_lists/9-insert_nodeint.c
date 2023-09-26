c65173b48dfe6bfd1cfb4f0f59023d6e7be871f2
Dimakatso Selebedi <selebedidimakatso083357@gmail.com>
2023-09-26 14:31:01 -0700

    function that deletes the node at index index of a listint_t linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			new_node->next = tmp->next;
+			tmp->next = new_node;
 			return (new_node);
 		}
 		else
