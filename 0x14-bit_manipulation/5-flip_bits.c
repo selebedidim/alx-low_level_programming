e24d28c807de580e65baa34e6dfe2acfb3e53d0e
Dimakatso <selebedidimakatso083357@gmail.com>
2023-10-26 11:55:09 -0700

    function that returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,10 +12,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int curr;
-	unsigned long int exclusive = n ^ m;
+	unsigned long int exclusive = n^m;
 
 
-	for (i = sizeof(unsigned long int) * 8; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
 		curr = exclusive >> i;
 		if (curr & 1)
