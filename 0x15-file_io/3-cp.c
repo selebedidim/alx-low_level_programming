d46d2cdae39cf9b8d63e23f50c0baeb9cea64d59
Dimakatso <selebedidimakatso083357@gmail.com>
2023-11-03 11:04:38 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void closefile(int fd);
+void close_file(int fd);
 
 /**
  * create_buffer - buffer allocation of 1024 bytes
@@ -13,19 +13,19 @@ void closefile(int fd);
  */
 char *create_buffer(char *file)
 {
-	char *buffer;
+	char *buff;
 
-	buffer = malloc(sizeof(char) * 1024);
+	buff = malloc(sizeof(char) * 1024);
 
-	if (buffer == NULL)
+	if (buff == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error:  can't write to %s\n", file);
 		exit(99);
 	}
-	return (buffer);
+	return (buff);
 }
 /**
- * close_file - Closes file decriptor
+ * close_file - Closes file descriptor
  * @fd:File descriptor to be closed
  */
 void close_file(int fd)
@@ -53,39 +53,39 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fdDest, fdSource, readBytes, writeBytes;
-	char buffer;
+	int file_from, file_to, read_byte, write_byte;
+	char *buff;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = create_buffer(argv[2]);
-	fdSource = open(argv[1], O_RDONLY);
-	readBytes = read(fdSource, (void *)buffer, 1024);
-	fdDest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buff = create_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	read_byte = read(file_from, buff, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (fdSource == -1 || readBytes == -1)
+		if (file_from == -1 || read_byte == -1)
 		{
-			dprintf(STDERR_FILENO, "Error:Can't read from source file %s\n", argv[1]);
-			free(buffer);
+			dprintf(STDERR_FILENO, "Error: can't read file_from file %s\n", argv[1]);
+			free(buff);
 			exit(98);
 		}
-		writeBytes = write(fdDest, (const void *)buffer, readBytes);
-		if (fdDest == -1 || writeBytes == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read to destination file %s\n" argv[2]);
-			free(buffer);
-			exit(99);
-	}
-	readBytes = read(fdSource, (void *)buffer, 1024);
-	fdDest = open(argv[2], O_WRONLY | O_APPEND);
-		} while (readBytes > 0);
-		free(buffer);
-		close_file(fdSource);
-		close_file(fdDest);
+		write_byte = write(file_to, buff, read_byte);
 
-		return (0);
+		if (file_to == -1 || write_byte == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			free(buff);
+			exit(99);
+		}
+		read_byte = read(file_from, buff, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (write_byte > 0);
+	free(buff);
+	close_file(file_from);
+	close_file(file_to);
+	return (0);
 }
